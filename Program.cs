34bdf83829f06b5494b0295ae8549d66dbd32b85
Ben Franklin <225748@live.brock.ac.uk>
2024-09-17 21:05:38 +0100

    added code to find file in debug folder and write it to a string

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace HTMLfromFileWithVariables
     {
         static void Main(string[] args)
         {
+            int adult = 2;
+            StreamReader htmlFile = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "/HTMLBookingConfirmation.html");
+            //To check file path - it should find this projects debug folder and then go to the file specified
+            //Console.WriteLine((AppDomain.CurrentDomain.BaseDirectory + "/HTMLBookingConfirmation.html").ToString());
+            //Console.ReadLine();
+            string html = htmlFile.ReadToEnd();
+            Console.WriteLine(html);
+            Console.ReadLine();
+
         }
     }
 }
