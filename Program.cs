e8d2dec7e33e15eb99f42c55aebf509b6d9b1223
Ben Franklin <225748@live.brock.ac.uk>
2025-02-13 18:21:41 +0000

    removed rotaconnect specific items

--- Program.cs
+++ Program.cs
@@ -15,27 +15,18 @@ namespace HTMLfromFileWithVariables
             //
 
             clsEmailManager emailManager = new clsEmailManager();
-            string templateFilePath = (AppDomain.CurrentDomain.BaseDirectory + "/Html_Email_Templates/newUserEmailTemplate.html");//directs to its own debug folder and then the file
+            string templateFilePath = (AppDomain.CurrentDomain.BaseDirectory + "/Html_Email_Templates/simple.html");//directs to its own debug folder and then the file
 
             //Make an array of variables to replace in the html template
-            clshtmlVariable[] variableReplacements = new clshtmlVariable[4];//num of unique variables in html template
-            clshtmlVariable htmlVariable;
+            clshtmlVariable[] variableReplacements = new clshtmlVariable[2];//num of unique variables in html template
 
             //for every unique variable in the template do this
-            //htmlVariable = new clshtmlVariable("{x}","12");
-            //variableReplacements[0] = htmlVariable;
-
-            //htmlVariable = new clshtmlVariable("{y}", "43");
-            //variableReplacements[1] = htmlVariable;
-
-            variableReplacements[0] = new clshtmlVariable("{firstName}", "Benjamin");
-            variableReplacements[1] = new clshtmlVariable("{lastName}", "Franklin");
-            variableReplacements[2] = new clshtmlVariable("{email}", "benjaminjfranklin99@gmail.com");
-            variableReplacements[3] = new clshtmlVariable("{temporaryPassword}", "12DOGTQ");
+            variableReplacements[0] = new clshtmlVariable("{x}", "12");
+            variableReplacements[1] = new clshtmlVariable("{y}", "43");
 
             string htmlBody = emailManager.ReadAndPopulateEmailTemplate(templateFilePath,variableReplacements);
 
-            emailManager.SendEmail("benjaminjfranklin99@gmail.com", htmlBody, "Your New Rota Connect Account!");
+            emailManager.SendEmail("benjaminjfranklin99@gmail.com", htmlBody, "Email Subject here");
             //improvement from old style is that the credentials file location no longer needs to be passed into this function with every call
             //the file location is now stored in the email manager class itself
 
